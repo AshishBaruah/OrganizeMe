535b1653503a219bd417fa1ceeab0d444e62bd15
Ashish Baruah <ashishbaruah90@gmail.com>
2025-03-06 23:03:31 +0530

    Added PageController , home and modified application properties file

--- src/main/java/com/scm/controllers/PageController.java
+++ src/main/java/com/scm/controllers/PageController.java
@@ -1,14 +1,22 @@
 package com.scm.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class PageController {
     //  to handle request
     @RequestMapping("/home")
-    public String home(){
+    public String home(Model model){
         System.out.println("Home page Handler");
+
+        //  sending data to view
+        model.addAttribute("name","Google Corporation");
+        model.addAttribute("YoutubeChannel","Learn Code with Ashish");
+        model.addAttribute("GithubRepo", "https://github.com/AshishBaruah/OrganizeMe");
+
+        //  key value pairs
         return "home";
     }
 }
